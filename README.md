d48f1b6187df11093d466cc633b08cfe417779c9
vchrisb <vchrisb@users.noreply.github.com>
2017-10-16 16:18:47 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # docker-homebridge-loxone
+
+This image runs [Homebridge with Loxone Plugin](https://github.com/Sroose/homebridge-loxone-ws).
+
+## How to use this image
+
+```sudo docker run --net=host -d --name homebridge -p 5353:5353 -p 51826:51826 -e LOXONE_IP_ADDRESS=192.168.1.200 -e LOXONE_PASSWORD='password' vchrisb/homebridge-loxone```
+
+The following environment variables are also honored for configuring your ScaleIO Gateway instance:
+* `-e LOXONE_IP_ADDRESS=` 
+* `-e LOXONE_USERNAME=` defaults to `admin`
+* `-e LOXONE_PASSWORD=` 
+* `-e HOMEBRIDGE_USERNAME=` defaults to `CA:AA:12:34:56:78`
+* `-e HOMEBRIDGE_PIN=` defaults to `012-34-567`
+
+### Examples
+
+```sudo docker run --net=host -d --name homebridge -p 5353:5353 -p 51826:51826 -e LOXONE_IP_ADDRESS=192.168.1.200 -e LOXONE_USERNAME="admin" -e LOXONE_PASSWORD='password' -e HOMEBRIDGE_USERNAME="CA:AA:12:34:56:78" -e HOMEBRIDGE_PIN="012-34-567" vchrisb/homebridge-loxone```
---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cd /root/.homebridge
+
+if [ "$HOMEBRIDGE_USERNAME" ]; then
+  jq --arg USERNAME $HOMEBRIDGE_USERNAME '.bridge.username=$USERNAME' config.json
+fi
+
+if [ "$HOMEBRIDGE_PIN" ]; then
+  jq --arg USERNAME $HOMEBRIDGE_PIN '.bridge.username=$PIN' config.json
+fi
+
+if [ "$LOXONE_IP_ADDRESS" ]; then
+  jq --arg IP_ADDRESS $LOXONE_IP_ADDRESS '(.platforms[] | select(.name == "Loxone").host) |= $IP_ADDRESS' config.json > config.json.tmp && mv config.json.tmp config.json
+else
+  echo "Please specify Loxone Address!"
+  exit
+fi
+
+if [ "$LOXONE_USERNAME" ]; then
+  jq --arg USERNAME $LOXONE_USERNAME '(.platforms[] | select(.name == "Loxone").username) |= $USERNAME' config.json > config.json.tmp && mv config.json.tmp config.json
+fi
+
+if [ "$LOXONE_PASSWORD" ]; then
+  jq --arg PASSWORD $LOXONE_PASSWORD '(.platforms[] | select(.name == "Loxone").password) |= $PASSWORD' config.json > config.json.tmp && mv config.json.tmp config.json
+else
+  echo "Please specify Loxone Password!"
+  exit
+fi
+
+if [ "$LOXONE_PORT" ]; then
+  jq --arg PORT $LOXONE_PORT '(.platforms[] | select(.name == "Loxone").port) |= $PORT' config.json > config.json.tmp && mv config.json.tmp config.json
+fi
+
+
+
+dbus-daemon --system
+avahi-daemon -D
+
+
+# Start Homebridge
+homebridge
+
